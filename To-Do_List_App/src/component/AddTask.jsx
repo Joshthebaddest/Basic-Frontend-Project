e03e2e5c7115dd31e4c0961533a0a7249136f60a
MARVO <gabrielmarvelous1@gmail.com>
2024-03-11 20:55:04 +0100

    third commit on todolist app

--- To-Do_List_App/src/component/AddTask.jsx
+++ To-Do_List_App/src/component/AddTask.jsx
@@ -4,9 +4,9 @@ import { useStateContext } from '../context/context';
 function AddTask() {
     const { taskDispatch, TaskData } = useStateContext()
     
-    const handleClick = (e) => {
+    const handleClick = (name) => {
       if(TaskData.length > 1){
-        taskDispatch({ type: 'ADDING_TASK', payload: { name: e.target.textContent  } })
+        taskDispatch({ type: 'ADDING_TASK', payload: { name } })
       }
     }
 
@@ -16,7 +16,7 @@ function AddTask() {
         <div className='border-2 border-solid text-center'>
             <ul className='border-2 border-solid'>
               {TaskData.map(item => (
-                <li key={item.id} className='py-2 border-b-2 border-solid cursor-pointer' onClick={handleClick}>{item.name} 
+                <li key={item.id} className='py-2 border-b-2 border-solid cursor-pointer' onClick={()=>handleClick(item.name)}>{item.name} 
                     <button className='mx-4'>{item.icon}</button>
                 </li>
               ))}
